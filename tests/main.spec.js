e747406d0e554c2ee5cdc19e75d3104c81c9c1be
wescleymartins <wescleymartins02@gmail.com>
2018-05-19 16:43:56 -0300

    Create spotify-wrapper smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,39 +1,32 @@
 import { expect } from 'chai';
-import FizzBuzz from '../src/main';
+import { search, searchAlbums, searchArtists, searchTracks, searchPlaylistis } from '../src/main';
 
-/*
-
-Desafio FizzBuzz
-
-Escreva uma lib que receba um número e:
-
-Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
-Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
-Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
-Se não for múltiplo de nada, retorna o número
-
- */
-
-describe('Main', ()=> {
-    it('should return Fizz when multiple of 3', () =>{
-        expect(FizzBuzz(3)).to.be.equal('Fizz')
-        expect(FizzBuzz(6)).to.be.equal('Fizz')
-    });
-    
-    it('should return Buzz when multiple of 5', () =>{
-        expect(FizzBuzz(5)).to.be.equal('Buzz')
-        expect(FizzBuzz(10)).to.be.equal('Buzz')
-    });
-    
-    it('should return FizzBuzz when multiple of 3 and 5', () =>{
-        expect(FizzBuzz(15)).to.be.equal('FizzBuzz')
-    });
-    
-    it('should return the number when non-multiple', () =>{
-        expect(FizzBuzz(7)).to.be.equal(7)
-    });
-    
-    it('should return 0 when 0', () =>{
-        expect(FizzBuzz(0)).to.be.equal(0)
-    });
+describe('Spotify Wrapper', () =>{
+  describe('smoke tests', () =>{
+      //search (Generico) - + de um tipo
+      //searchAlbums
+      //searchArtists
+      //searchTracks
+      //searchPlaylistis
+      
+      it('should exist the search method', () =>{
+          expect(search).to.exist;
+      });
+      
+      it('should exist the searchAlbums method', () =>{
+          expect(searchAlbums).to.exist;
+      });
+      
+      it('should exist the searchArtists method', () =>{
+          expect(searchArtists).to.exist;
+      });
+      
+      it('should exist the searchTracks method', () =>{
+          expect(searchTracks).to.exist;
+      });
+      
+      it('should exist the searchPlaylistis method', () =>{
+          expect(searchPlaylistis).to.exist;
+      });
+  })  
 })
