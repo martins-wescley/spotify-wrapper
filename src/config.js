749289754cf4fb05bbe8ca8d380eb7559c8d2a0b
martins-wescley <wescleymartins02@gmail.com>
2018-05-23 13:21:57 -0300

    Get a Json to test

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const TOKEN_API = 'BQA9-pFgvtsi49dCiODiI7oWuiRlWe16eU1-I8PNUiXI4aA4YHS3Le6RI4DPDRVr6q5ZSq8WSxImbEPCZf-xoQ_H_tCFWNpWd2_WrGNWVc_xE3mNBzbbBZ23_k6-t0uDQR7BGlbkqdpek7rz';
+const TOKEN_API = 'BQDQeMRZSTeHGerj3MNfD3uOn0RVUglxebHUDDb63BVlFyizTAU5lR6mkZro2l5o0Inv6WZvzsXDTX7lvOuA2urLV8KV6eZm_RioPfpTxviGA6M4ANKq_muecgKNp6LnWazkR7vAIPv-mjCx';
 
 
 export const API_URL = 'https://api.spotify.com/v1/';
